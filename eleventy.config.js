fb9f19153a7de01b385d99be1c731358c92b8fad
Matthew Miller <mmiller@duosecurity.com>
2026-01-27 13:12:13 -0800

    Add some asset files

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,5 +5,12 @@ export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory(inputPath);
   eleventyConfig.setOutputDirectory("./_site");
 
+  // Assets for the homepage
   eleventyConfig.addPassthroughCopy(`${inputPath}/assets/`);
+  eleventyConfig.addPassthroughCopy(`${inputPath}/*.{png,txt}`);
+
+  // Tool-specific assets
+  eleventyConfig.addPassthroughCopy(`${inputPath}/featuredetect/**/*.{png,txt}`);
+  eleventyConfig.addPassthroughCopy(`${inputPath}/relatedorigins/**/*.{js,}`);
+  eleventyConfig.addPassthroughCopy(`${inputPath}/responsedecoder/**/*.{js,}`);
 }
